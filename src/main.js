ae426bba13c337a3dc1e2da4fc32c7e4eacab36d
Baxtiyor Erkinov <baxa10th@gmail.com>
2022-10-25 18:01:23 +0500

    .

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
+import * as $ from 'jquery'
 import clickFn from "./logger"
 import json from './assets/json'
 import Png from './assets/webpack-logo.png'
+import xml from './assets/data.xml'
+import csv from './assets/data.csv'
+
+
 import "./styles/style.css"
 
 console.log("run")
@@ -9,7 +14,12 @@ export default function consoleLogName() {
   alert("qweq")
 }
 
+console.log($)
+
 console.log(json)
 console.log(Png);
+console.log(xml)
+console.log(csv)
+
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,28 @@
 const path = require('path')
 const HTMLWebpackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetWebpackPlugin = require("optimize-css-assets-webpack-plugin")
+const TerserWebpackPlugin = require("terser-webpack-plugin")
+
+const isDev = process.env.NODE_ENV === "development"
+const isProd = !isDev
+
+const optimization = () => {
+  const conf = {
+    splitChunks: {
+      chunks: 'all'
+    }
+  }
+  if (isProd) {
+    conf.minimizer = [
+      new OptimizeCssAssetWebpackPlugin(),
+      new TerserWebpackPlugin()
+    ]
+  }
+
+  return conf
+}
 
 module.exports = {
   mode: "development",
@@ -16,21 +38,45 @@ module.exports = {
   plugins: [
     new HTMLWebpackPlugin({
       title: "App",
-      template: './src/index.html'
+      template: './src/index.html',
+      minify: {
+        collapseWhitespace: isProd
+      }
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    })
   ],
+  optimization: optimization(),
+  devServer: {
+    port: 7777,
+    hot: isDev,
+    client: {
+      progress: true,
+    },
+  },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader']
       }, {
         test: /\.png/,
         type: 'asset/resource'
       },
       {
         test: /\.ttf/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.xml$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.csv$/,
         type: 'asset/resource'
       }
     ]
